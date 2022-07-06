965bfd48ad95bd6b1383ea267db8f9d38aa65b82
Brad <tlonghorn790@gmail.com>
2022-07-07 02:13:55 +0300

    First Commit

---
+++ vite.config.js
@@ -0,0 +1,13 @@
+import path from 'path'
+import glob from 'glob'
+
+export default {
+  // root: path.join(__dirname, 'src'),
+  // build: {
+  //   outDir: path.join(__dirname, 'dist'),
+  //   rollupOptions: {
+  //     input: glob.sync(path.resolve(__dirname, 'src', 'views/*.html')),
+  //   },
+  // },
+  emptyOutDir: true,
+}
